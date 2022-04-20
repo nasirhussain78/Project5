94435b65996940e53a3f5204bbf59748a4da83f2
Aman Pandey <amandbest786@gmail.com>
2022-04-20 10:46:21 +0530

    project 5

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -44,9 +44,17 @@ const createProduct = async (req, res) => {
                 }
             }
 
-            if (!validator.isValidValue(availableSizes)) {
-                return res.status(400).send({ status: false, messege: "please provide availableSizes" })
+            let sizeEnum = availableSizes.split(",").map(x => x.trim())
+            if(req.body.hasOwnProperty('availableSizes')){
+            if(!validator.isValidValue(availableSizes)){
+                return res.status(400).send({status:false, message:"A valid Size should present"})
             }
+            for (let i = 0; i < sizeEnum.length; i++) {
+                if (!(["S", "XS", "M", "X", "L", "XXL", "XL"].includes(sizeEnum[i]))) {
+                    return res.status(400).send({status: false, message: 'Available Sizes must be ${["S", "XS", "M", "X", "L", "XXL", "XL"]}' })
+                }
+            }
+        }
 
         currencyFormat = currencySymbol('INR') 
         
